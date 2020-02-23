c14979d093a25ac012ce10679355229b92714967
Hemin Johnson <achayan1992@gmail.com>
2020-02-23 22:12:01 +0100

    feat: wrap text central to the app bar

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I Am Rich'),
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
